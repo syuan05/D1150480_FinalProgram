65089a32feb09e725ec7a1be66f4fe3bb7dd8642
syuan05 <syuan930510@gmail.com>
2024-06-04 21:00:49 +0800

    0604

---
+++ into.js
@@ -0,0 +1,23 @@
+var clickCounts = [0, 0, 0, 0, 0, 0];
+
+function photoClicked(index) {
+    clickCounts[index]++;
+    document.getElementById("clickCount" + index).innerText = "投票: " + clickCounts[index];
+}
+
+function resetVotes() {
+    clickCounts = [0, 0, 0, 0, 0, 0];
+    for (var i = 0; i < 6; i++) {
+        document.getElementById("clickCount" + i).innerText = "投票: 0";
+    }
+}
+
+for (var i = 0; i < 6; i++) {
+    document.getElementById("photo" + (i + 1)).innerHTML += "<br><div class='click-count' id='clickCount" + i + "'>投票: 0</div>";
+    var imgElement = document.querySelector("#photo" + (i + 1) + " img");
+    imgElement.addEventListener("click", (function (index) {
+        return function () {
+            photoClicked(index);
+        };
+    })(i));
+}
